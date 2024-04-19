f2d77bfa2ed13f3782797ff36321751b83e52919
Jawed Sheikh <156115233+Arhamjawed66@users.noreply.github.com>
2024-04-19 21:51:19 +0500

    study java scritpe

---
+++ funtion.js
@@ -0,0 +1,23 @@
+// let n1 = 10;
+// let n2 = 10;
+// let sign = "-";
+// if(sign === "+"){
+//  console.log(n1 + n2);
+//  }
+// else if(sign === "-"){
+//     console.log(n1 - n2);
+//     }
+// else if(sign === "*"){
+//         console.log(n1 * n2);    }
+// else if(sign === "/"){
+//      console.log(n1 / n2);
+//     }
+// else{
+//  console.log("invalid sign");
+// } 
+var myfun = function (n1, n2, sign) {
+    if (sign === "+") {
+        console.log(n1 + n2);
+    }
+};
+myfun(10, 5, "+");
---
+++ funtion.ts
@@ -0,0 +1,25 @@
+// let n1 = 10;
+// let n2 = 10;
+// let sign = "-";
+
+// if(sign === "+"){
+//  console.log(n1 + n2);
+//  }
+// else if(sign === "-"){
+//     console.log(n1 - n2);
+//     }
+// else if(sign === "*"){
+//         console.log(n1 * n2);    }
+// else if(sign === "/"){
+//      console.log(n1 / n2);
+//     }
+// else{
+//  console.log("invalid sign");
+// } 
+
+let myfun = (n1:number,n2:number,sign:string) => {
+    if(sign === "+"){
+  console.log(n1 + n2);
+}  
+}
+ myfun (10, 5 ,"+")
